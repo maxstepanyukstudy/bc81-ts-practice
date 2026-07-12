a6f07ed5c8e5ca8784bf7482538aaed30608a2dd
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-12 23:05:24 +0300

    module-01-1-primitives - fix example

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "./style.css";
 
-// import './modules/module-01-1-primitives.ts'
+import './modules/module-01-1-primitives.ts'
 // import './modules/module-01-2-objects.ts'
 // import "./modules/module-01-3-interface.ts";
 // import "./modules/module-01-4-array.ts";
 // import "./modules/module-01-4-array.js";
 // import "./modules/module-01-5-custom-type.ts";
-import "./modules/module-01-5-functon.ts";
+// import "./modules/module-01-5-functon.ts";
