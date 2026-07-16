d67026f6bc830392902534ac4c33a29d2528b841
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-16 19:20:01 +0300

    enable all* modules

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import "./style.css";
 
-// import './modules/module-01-1-primitives.ts'
-// import './modules/module-01-2-objects.ts'
-// import "./modules/module-01-3-interface.ts";
-// import "./modules/module-01-4-array.ts";
+import './modules/module-01-1-primitives.ts'
+import './modules/module-01-2-objects.ts'
+import "./modules/module-01-3-interface.ts";
+import "./modules/module-01-4-array.ts";
 // import "./modules/module-01-4-array.js";
-// import "./modules/module-01-5-custom-type.ts";
-// import "./modules/module-01-5-functon.ts";
+import "./modules/module-01-5-custom-type.ts";
+import "./modules/module-01-5-functon.ts";
 
-// import "./modules/module-01-6-enum.ts";
-// import "./modules/module-01-7-generics.ts";
-// import "./modules/module-01-8-promice.ts";
+import "./modules/module-01-6-enum.ts";
+import "./modules/module-01-7-generics.ts";
+import "./modules/module-01-8-promice.ts";
 import "./modules/module-01-9-http-1-fetch.ts";
 import "./modules/module-01-9-http-2-axios.ts";
